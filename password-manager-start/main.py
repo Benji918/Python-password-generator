15dbe8063568a20146a9001d2a3e2977a638f1fe
Benjamin <70521283+Benji918@users.noreply.github.com>
2022-07-12 17:18:30 -0700

    Update main.py

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,7 +2,26 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 FONT_NAME = "Courier"
+
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def find_password():
+    try:
+        with open('data.jason', mode='r') as data_file:
+            data_dic = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showwarning(title='File not found', message='No data file found!')
+    else:
+        if web_entry.get() in data_dic:
+            data_email = data_dic[web_entry.get()]['email']
+            data_pwd = data_dic[web_entry.get()]['password']
+            messagebox.showinfo(title=web_entry.get(), message=f'Email: {data_email}\nPassword: {data_pwd}')
+        else:
+            messagebox.showinfo(title='Not found', message='No details for website exits!')
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def fill_password():
@@ -25,21 +44,34 @@ def fill_password():
     # window popup to tell the user password has being copied
     messagebox.showinfo(title="Copied to Clipboard!", message="Password copied to clipboard.")
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
+    anew_data = {
+        web_entry.get(): {
+            'email': email_entry.get(),
+            'password': pwd_entry.get(),
+        }
+    }
     # message popup to check if any field are empty
     if len(web_entry.get()) == 0 or len(pwd_entry.get()) == 0:
         messagebox.showwarning(title='Empty field found', message='Hey!\nAll fields need to be filled!')
     else:
-        # message popup to confirm data entered
-        is_ok = messagebox.askokcancel(title=web_entry.get(),
-                                       message=f'These are the details entered\n Email: {email_entry.get()}\n Password: {pwd_entry.get()}\n Is it ok to save?')
-        if is_ok:
-            with open('data.txt', 'a') as data:
-                data.write(
-                    f'Website: {web_entry.get()}| Email: {email_entry.get()}| Password: {pwd_entry.get()}' + '\n')
-                web_entry.delete(0, 'end')
-                pwd_entry.delete(0, 'end')
+        try:
+            with open('data.jason', 'r') as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            with open('data.jason', 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Update old data with new data
+            data.update(new_data)
+            with open('data.jason', 'w') as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            web_entry.delete(0, 'end')
+            pwd_entry.delete(0, 'end')
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -84,6 +116,10 @@ pwd_entry.grid(column=1, row=3, sticky='ew')
 # button
 button = Button(text='Generate password', command=fill_password)
 button.grid(row=3, column=2, sticky='ew')
+
+# search
+button = Button(text='search', command=find_password)
+button.grid(row=1, column=2, sticky='ew')
 
 # add button
 add = Button(text='Add', width=36, command=save)
